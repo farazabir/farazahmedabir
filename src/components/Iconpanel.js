840b687a1c2b6c7e7684b7732dc8ca7ac79a8ff3
Faraz ahmed abir <farazabir50@gmail.com>
2023-03-03 23:26:08 +0600

    new

--- src/components/Iconpanel.js
+++ src/components/Iconpanel.js
@@ -1,4 +1,4 @@
-import { faFacebookF, faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function IconPanel() {
@@ -11,17 +11,17 @@ function IconPanel() {
           </a>
         </li>
         <li className="list-inline-item">
-          <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} size="2x" className="text-black m-3" />
+          <a href="https://github.com/farazabir" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" className="text-black m-3" />
           </a>
         </li>
         <li className="list-inline-item">
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/farazahamedabir/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} size="2x" className="text-black m-3" />
           </a>
         </li>
         <li className="list-inline-item">
-          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/faraz-ahmed-abir-57167a1ab/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-black m-3" />
           </a>
         </li>
