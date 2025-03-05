c06a246d2387affdb5606c02c99f2b1b3d5ca05b
Faraz Ahmed Abir <62275863+farazabir@users.noreply.github.com>
2025-03-05 10:38:46 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/farazahmedabir/",
+  basePath: "/farazahmedabir",
   images: {
     unoptimized: true,
     remotePatterns: [
